a4e3dcdd9a4c6089fc898b9d05cbd1666d4fb010
colton.murphy <cmurp77@gmail.com>
2026-07-27 10:46:13 -0400

    wip

--- test/src/Program.cs
+++ test/src/Program.cs
@@ -1,25 +1,9 @@
-using CeetemSoft.Io.Ftdi;
-
 namespace Test;
 
 public static partial class Program
 {
 	public static void Main(string[] args)
 	{
-		var device = FtdiDevice.GetFirstDevice();
-
-		if (device == null)
-		{
-			return;
-		}
-                                                                                                                                                                                                                                                                                                                                                      
-		device.Open();
-		device.SetFlowControl();
-		device.SetBaudRate(3000000);
-		device.SetDataCharacteristics();
-		device.Write([0xA5, 0x42, 0x55]);
-
-
-		device.Close();
+		
 	}
 }
